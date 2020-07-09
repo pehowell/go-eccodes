1e49be6b37ea67f0562ef3fa193980bf2046e767
Paul Howell <paul.howell@gmail.com>
2020-07-08 19:13:48 -0700

    Added macos compile flags

---
+++ native/plus_darwin_amd64.go
@@ -0,0 +1,15 @@
+// +build darwin,amd64
+
+package native
+
+/*
+#cgo LDFLAGS: -leccodes -lpng -laec -lopenjp2 -lz -lm
+//removed: -leccodes_memfs -ljasper
+*/
+import "C"
+
+type Cint = int32
+type Clong = int64
+type Culong = uint64
+type Cdouble = float64
+type CsizeT = int64
